c4c70a798003d81da92440e552db9d055f83c2d0
KhubiAubergine <khubi@aubergine.co>
2025-08-05 01:30:06 +0530

    button layout on mobile

--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -17,7 +17,7 @@ export function WhatsAppButton({
     )}?text=${encodeURIComponent(message)}`;
 
     return (
-        <div className='fixed bottom-6 right-6 z-50'>
+        <div className='fixed bottom-10 md:bottom-6 right-6 z-50'>
             <Link
                 href={whatsappUrl}
                 className='text-green-500 hover:text-green-600 transition-all duration-300 hover:scale-110 group'
